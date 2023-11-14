1f90fb87650ce2f0f3dbd2fdb7c242d8f921cc1d
hel99 <helena.aganovic99@gmail.com>
2023-11-14 16:39:32 +0100

    dodavanje if-a za ceo broj

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const zeljeniBroj = Math.floor((Math.random() * 10) + 1);
 
 function prikaziPoruku(poruka) {
-    document.getElementById('message').textContent = poruka;
+    document.getElementById("message").textContent = poruka;
 }
 
 function proveraBroja() {
-    const unetiBroj = parseInt(document.getElementById('guess').value);
-    if (unetiBroj >= 1 && unetiBroj <= 10) {
+    const unetiBroj = parseFloat(document.getElementById("guess").value);
+    if (Number.isInteger(unetiBroj)) {
+        if (unetiBroj >= 1 && unetiBroj <= 10) {
         if (unetiBroj === zeljeniBroj) {
-            prikaziPoruku('Bravo, care.');
+            prikaziPoruku("Bravo, care.");
         } 
         else {
-            prikaziPoruku('Nisi pogodio. Zeljeni broj je ' + zeljeniBroj);
+            prikaziPoruku("Nisi pogodio.");
         }
     }
     else {
-        prikaziPoruku('Unesi broj od 1 do 10.');
+        prikaziPoruku("Unesi broj od 1 do 10.");
     }
+    } else {
+        prikaziPoruku("Unesi ceo broj.")
+    }
+    
 }
-
-
 
